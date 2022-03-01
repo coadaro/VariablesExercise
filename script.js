60f8617fe0c0489722b1d9cb7cfc9808a1c780aa
“coadaro” <coadaro@gmail.com>
2022-03-01 16:48:12 -0500

    VariablesExerciseWithBonus

---
+++ script.js
@@ -0,0 +1,15 @@
+//const
+let num=20
+num=15
+num+=7
+num--
+num='This is not a number'
+let blank;
+console.log(blank);
+blank=null
+
+//bonus
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01
+console.log(quarter, dime, nickel, penny);
+let letters='qwertyuiop'
+console.log(letters.indexOf('p'));
