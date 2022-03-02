644c046ae794500e73995ebbd08e2c5c42f8b275
“coadaro” <coadaro@gmail.com>
2022-03-02 10:01:04 -0500

    ExtraBonus

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 //const
-let num=20
-num=15
-num+=7
-num--
-num='This is not a number'
+let num=20;
+num=15;
+num+=7;
+num--;
+num='This is not a number';
 let blank;
 console.log(blank);
-blank=null
+blank=null;
 
 //bonus
-const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
 console.log(quarter, dime, nickel, penny);
-let letters='qwertyuiop'
-console.log(letters.indexOf('p'));
+let letters='qwertyuiop';
+
+console.log(letters.length);
+console.log(letters[9]);
+//or
+console.log(letters.slice(9));
+//or
+console.log(letters[letters.length-1]);
